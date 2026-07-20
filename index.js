86b56ff5c45fff081bd678c843d585df852e89fb
rohitkhati9053-debug <rohitkhati9053@gmail.com>
2026-07-20 11:42:56 +0530

    remove extra comments

--- index.js
+++ index.js
@@ -14,32 +14,13 @@ app.use(express.urlencoded({ extended: true }));
 
 MovieRoutes(app)   //invoking movie routes
 theatreRoutes(app)   //invoking theatre routes
-// Route
-app.get("/home", (req, res) => {
-    res.status(200).json({
-        success: true,
-        message: "Fetched Home"
-    });
-});
+
 
 // Database Connection
 async function connectDB() {
     try {
         await mongoose.connect(process.env.DB_URL);
         console.log("✅ MongoDB Connected");
-        // const createdMovie=await Movie.create({
-        //     name:"Boogi Boogi",
-        //     description:"comedy",
-        //     casts:["akshay","kirti"],
-        //     director:"farhas",
-        //     trailerUrl:"https://boody.com",
-        //     language:"Hindi",
-        //     releaseDate:"18-01-2026",
-        //     releaseStatus:"RELEASED"
-        // })
-        // console.log("createdMdie: ",createdMovie);
-        
-
         app.listen(process.env.PORT, () => {
             console.log(`🚀 Server running on port ${process.env.PORT}`);
         });
